2f4cd3a16fa46c463a1d67a700ebae4fa3c1eacb
Karina Bora <96630595+karinabora@users.noreply.github.com>
2023-06-19 12:14:11 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,26 @@
+var box=document.getElementById("mydiv")
+console.log(box)
+
+var purple=document.getElementById("red")
+red.onclick=function(){
+box.style.backgroundColor="red"
+}
+console.log(red)
+
+var aqua=document.getElementById("green")
+aqua.onclick=function(){
+box.style.backgroundColor="green"
+}
+console.log(green)
+
+
+var yellow=document.getElementById("pink")
+pink.onclick=function(){
+box.style.backgroundColor="pink"
+}
+console.log(pink)
+
+
+
+
+
